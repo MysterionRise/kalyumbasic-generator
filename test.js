a7c7552d9e613afcd8014a5b7f5a09e91546afeb
Konstantin Perikov <MysterionRise@users.noreply.github.com>
2016-04-18 14:14:11 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,15 @@
-(function() {
+function removeMayonnaise() {
 var elems = document.getElementsByTagName("div"),
    i;
 for (i in elems) {
    if (elems[i].className == "wrapped") {
       var innerDiv = elems[i].getElementsByTagName("div");
       if (innerDiv[0].getElementsByTagName("a")[0].href.indexOf("/rajonchik37") > 0) {
-         innerDiv[1].innerHTML = "***********************";
+
+         innerDiv[1].innerHTML = Array(Math.floor((Math.random() * 100) + 1)).join("*");
       }
    }
 }
-})();
+}
+
+removeMayonnaise();
