32134aef107503e2ff0c6f9a8a3a248ce87d15bc
Konstantin Perikov <MysterionRise@users.noreply.github.com>
2016-04-18 14:48:48 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,22 @@
-function removeMayonnaise() {
-var elems = document.getElementsByTagName("div"),
-   i;
-for (i in elems) {
-   if (elems[i].className == "wrapped") {
+function removeMayonnaiseFromVK() {
+  var elems = document.getElementsByTagName("div"),
+    i;
+  for (i in elems) {
+    if (elems[i].className == "wrapped") {
       var innerDiv = elems[i].getElementsByTagName("div");
       if (innerDiv[0].getElementsByTagName("a")[0].href.indexOf("/rajonchik37") > 0) {
 
-         innerDiv[1].innerHTML = Array(innerDiv[1].innerHTML.length + 1).join("*");
+        innerDiv[1].innerHTML = Array(innerDiv[1].innerHTML.length + 1).join("*");
       }
-   }
-}
+    }
+  }
 }
 
-removeMayonnaise();
-setInterval(removeMayonnaise, 1000);
+function removeMayonnaiseFromVK() {
+  
+}
+
+removeMayonnaiseFromVK();
+removeMayonnaiseFromSports();
+setInterval(removeMayonnaiseFromVK, 1000);
+setInterval(removeMayonnaiseFromSports, 1000);
