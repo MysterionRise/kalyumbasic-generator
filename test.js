e0d85e1639d5cc7ac20c9ca8f6294ede7e205c72
Konstantin Perikov <MysterionRise@users.noreply.github.com>
2016-04-18 15:29:06 +0200

    Update test.js

--- test.js
+++ test.js
@@ -4,8 +4,7 @@ function removeMayonnaiseFromVK() {
   for (i in elems) {
     if (elems[i].className == "wrapped") {
       var innerDiv = elems[i].getElementsByTagName("div");
-      if (innerDiv[0].getElementsByTagName("a")[0].href.indexOf("/rajonchik37") > 0) {
-
+      if (innerDiv[0].getElementsByTagName("a")[0].href.indexOf("/rajonchik37") > 0 || innerDiv[0].getElementsByTagName("a")[0].href.indexOf("/anotherID") > 0) {
         innerDiv[1].innerHTML = Array(innerDiv[1].innerHTML.length + 1).join("*");
       }
     }
