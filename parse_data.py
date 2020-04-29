ee5c1e85af0608c58de0b419a3e1da70731ba2dc
Konstantin Perikov <pericov.ko@gmail.com>
2020-04-29 15:19:19 +0100

    add parsing of data, storing as csv

---
+++ parse_data.py
@@ -0,0 +1,22 @@
+import vk
+import pandas as pd
+
+if __name__ == "__main__":
+    token = open('config.env', 'r').readline().strip()
+    session = vk.Session(access_token=token)
+    vk_api = vk.API(session)
+    nakur = True
+    offset = 0
+    records = []
+    while nakur:
+        wall = vk_api.wall.get(v="5.00", domain="kalikfan", offset=offset, count=100)
+        if len(wall["items"]) != 100:
+            nakur = False
+        for item in wall["items"]:
+            records.append(item["text"])
+        offset += 100
+    print(len(records))
+    df = pd.DataFrame(records, columns=['text'])
+    df.to_csv('data.csv')
+
+
