4ee378b05ae029c57d94c15f32c2c8edb3bb370c
klydd <klydd@mail.ru>
2020-08-29 16:22:22 +0300

    Add likes and links filter, extract magic numbers

--- parse_data.py
+++ parse_data.py
@@ -1,4 +1,5 @@
 import vk
+import re
 import pandas as pd
 
 import settings
@@ -7,22 +8,26 @@ import settings
 def read_data(output_f: str):
     session = vk.Session(access_token=settings.VK_AUTH_TOKEN)
     vk_api = vk.API(session)
-    nakur = True
+    api_version = "5.21"
     group_domain = "kalikfan"
+    url_regex = r"[a-zA-Z]+\.[a-zA-Z]+"
+    nakur = True
     offset = 0
+    parse_count = 100  # Count of posts/comments at request
+    likes_needed = 10
     records = []
-    group = vk_api.groups.getById(v="5.21", group_ids=group_domain)[0]
+    group = vk_api.groups.getById(v=api_version, group_ids=group_domain)[0]
     group_id = "-" + str(group["id"])  # Group id must starts with "-"
 
     while nakur:
-        wall = vk_api.wall.get(v="5.21", domain=group_domain, offset=offset, count=100)
+        wall = vk_api.wall.get(v=api_version, domain=group_domain, offset=offset, count=parse_count)
 
-        if len(wall["items"]) != 100:
+        if len(wall["items"]) != parse_count:
             nakur = False
         for item in wall["items"]:
             text = item["text"]
             # Skip ads
-            if "#калик_рекламик" in text or "://" in text:
+            if "#калик_рекламик" in text or "://" in text or re.search(url_regex, text):
                 continue
             records.append(item["text"])
 
@@ -31,22 +36,24 @@ def read_data(output_f: str):
             comments_offset = 0
             post_comments = []
             while comments_nakur:
-                get_comments = vk_api.wall.getComments(v="5.21", owner_id=group_id, post_id=post_id, need_likes=1, count=100, offset=comments_offset)
+                get_comments = vk_api.wall.getComments(v=api_version, owner_id=group_id, post_id=post_id, need_likes=1,
+                                                       count=parse_count, offset=comments_offset)
                 post_comments += get_comments["items"]
-                if len(get_comments["items"]) != 100:
+                if len(get_comments["items"]) != parse_count:
                     comments_nakur = False
-                comments_offset += 100
+                comments_offset += parse_count
 
-            # Filter comments by likes count:
+            # Filter and sort comments by likes count:
+            post_comments = [com for com in post_comments if com["likes"]["count"] > likes_needed]
             post_comments = sorted(post_comments, key=lambda k: k["likes"]["count"], reverse=True)
             # Add most popular comments
-            for comment in post_comments[:3]:
+            for comment in post_comments[:5]:
                 comment_text = comment["text"]
                 # In case of answers to comment or links
-                if "[id" in comment_text or "://" in comment_text:
+                if "[id" in comment_text or "://" in comment_text or re.search(url_regex, comment_text):
                     continue
                 records.append(comment_text)
-        offset += 100
+        offset += parse_count
     print(len(records))
     df = pd.DataFrame(records, columns=['text'])
     df.to_csv(output_f)
